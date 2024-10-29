e41d63c111600907692314273e3890ca66ca37c9
Arne-homework <>
2024-10-29 11:44:59 +0100

    iniital commit

---
+++ src/agd_trees/AGDTreesApplication.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package agd_trees;
+
+/**
+ * 
+ */
+public class AGDTreesApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
